3de2f1ec097150040b284e32c0b79289891a2045
Andrea Burattin <andrea.burattin@gmail.com>
2025-11-21 10:10:59 +0100

    Added buttons to test, and fixed facade

--- public/index.js
+++ public/index.js
@@ -1,8 +1,19 @@
+import "../lib/assets/petrinet-io.css";
 import PetriNetIO from '../lib/PetriNetIO'; // or from 'petrinet-io' after install
 
-new PetriNetIO({
+const petrinetio = new PetriNetIO({
   container: '#container'
 });
 
 
+document.getElementById('js-open-pnml').addEventListener('click', () => {
+  petrinetio.loadFromFile();
+});
 
+document.getElementById('js-download-pnml').addEventListener('click', () => {
+  petrinetio.exportPNML();
+});
+
+document.getElementById('js-download-svg').addEventListener('click', () => {
+  petrinetio.exportSVG();
+});
